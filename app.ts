8ca7d869ef654c109584aeea544667033e943ded
david-commit <davidondiege@gmail.com>
2023-10-05 23:45:11 +0300

    ft: Reinstall Prisma

--- app.ts
+++ app.ts
@@ -3,15 +3,16 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
 import morgan from 'morgan'
-
+import cors from 'cors'
 
 // Configs
 const app = express();
 dotenv.config();
 
 // Middlewares
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors())
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan('dev'));
 
 // Routes
