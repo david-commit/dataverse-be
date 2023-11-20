18b1b221dabf065b879ea407d24d0989b0140c34
david-commit <davidondiege@gmail.com>
2023-11-20 04:54:51 +0300

    ft: Added seed data wth functions

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,29 +1,72 @@
 import { db } from '../src/utils/db.server';
-import { userData } from './seedData';
+import { testAdminData, testCareerData, testBlogData } from './seedData';
 
-type UserType = {
+type AdminType = {
   name: string;
   email: string;
   password: string;
   phone: string;
 };
 
+type JobType = {
+  job_title: string;
+  job_description: string;
+  job_details: string;
+  postedBy: string;
+};
+
+type BlogType = {
+  blog_title: string;
+  blog_description: string;
+  blog_image: string;
+  blog_details: string;
+  postedBy: string;
+};
+
+// ==== MAIN SEED FUNCTION ====
 const seed = async () => {
-  // Seed Users
+  //==> Seed Admins
   await Promise.all(
-    userData.map((user: UserType) => {
-      return db.user.create({
+    testAdminData.map((admin: AdminType) => {
+      return db.admin.create({
         data: {
-          name: user.name,
-          email: user.email,
-          password: user.password,
-          phone: user.phone,
+          name: admin.name,
+          email: admin.email,
+          password: admin.password,
+          phone: admin.phone,
         },
       });
     })
   );
 
-  // Seed
+  //==> Seed Jobs
+  await Promise.all(
+    testCareerData.map((job: JobType) => {
+      return db.job.create({
+        data: {
+          job_title: job.job_title,
+          job_description: job.job_description,
+          job_details: job.job_details,
+          postedBy: job.postedBy,
+        },
+      });
+    })
+  );
+
+  //==> Seed Blogs
+  await Promise.all(
+    testBlogData.map((blog: BlogType) => {
+      return db.blog.create({
+        data: {
+          blog_title: blog.blog_title,
+          blog_description: blog.blog_description,
+          blog_image: blog.blog_image,
+          blog_details: blog.blog_details,
+          postedBy: blog.postedBy,
+        },
+      });
+    })
+  );
 };
 
 seed();
--- prisma/seedData.ts
+++ prisma/seedData.ts
@@ -1,4 +1,4 @@
-export const userData = [
+export const testAdminData = [
   {
     name: 'David',
     email: 'd@d.com',
@@ -16,5 +16,53 @@ export const userData = [
     email: 'f@f.com',
     password: 'password',
     phone: '0712345678',
+  },
+];
+
+export const testCareerData = [
+  {
+    job_title: 'System Administrator',
+    job_description: 'lorem ipsum dolor sit amet, consectetur adip',
+    job_details: 'HTML code here',
+    postedBy: 'David',
+  },
+  {
+    job_title: 'Systems Analyst',
+    job_description: 'lorem ipsum dolor sit amet, consectetur adip',
+    job_details: 'HTML code here',
+    postedBy: 'Lynette',
+  },
+  {
+    job_title: 'Data Analyst',
+    job_description: 'lorem ipsum dolor sit amet, consectetur adip',
+    job_details: 'HTML code here',
+    postedBy: 'Faith',
+  },
+];
+
+export const testBlogData = [
+  {
+    blog_title: 'How to clean your data',
+    blog_description: 'We teach you how to clean your data',
+    blog_image:
+      'https://www.simplilearn.com/ice9/free_resources_article_thumb/What_is_Data_Types_of_Data_and_How_To_Analyze_Data.jpg',
+    blog_details: 'HTML code goes here',
+    postedBy: 'David',
+  },
+  {
+    blog_title: 'Improve data collection',
+    blog_description: 'We teach you how to collect your data',
+    blog_image:
+      'https://www.simplilearn.com/ice9/free_resources_article_thumb/What_is_Data_Types_of_Data_and_How_To_Analyze_Data.jpg',
+    blog_details: 'HTML code goes here',
+    postedBy: 'Lynette',
+  },
+  {
+    blog_title: 'How to analyze your data',
+    blog_description: 'We teach you how to analyze your data',
+    blog_image:
+      'https://www.simplilearn.com/ice9/free_resources_article_thumb/What_is_Data_Types_of_Data_and_How_To_Analyze_Data.jpg',
+    blog_details: 'HTML code goes here',
+    postedBy: 'Faith',
   },
 ];
