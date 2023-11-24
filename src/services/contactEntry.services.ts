2a71760ca8a1876d383e38a3cdd5954a53c08966
david-commit <davidondiege@gmail.com>
2023-11-24 23:19:24 +0300

    ft: Created Contac Form db service

---
+++ src/services/contactEntry.services.ts
@@ -0,0 +1,65 @@
+import { db } from '../utils/db.server';
+
+// =================================================================
+//        GET: All Contact Entries
+// =================================================================
+export const getAllContactEntriesService = async () => {
+  return await db.contactEntry.findMany({
+    select: {
+      id: true,
+      name: true,
+      phone: true,
+      email: true,
+      subject: true,
+      message: true,
+    },
+  });
+};
+
+// =================================================================
+//        GET: Single Contact Entry
+// =================================================================
+export const getContactEntryService = async (contactEntryID: number) => {
+  return await db.contactEntry.findUnique({
+    where: {
+      id: contactEntryID,
+    },
+    select: {
+      id: true,
+      name: true,
+      phone: true,
+      email: true,
+      subject: true,
+      message: true,
+      createdAt: true,
+      updatedAt: true,
+    },
+  });
+};
+
+// =================================================================
+//        POST: New Contact Entry
+// =================================================================
+type NewContactEntryType = {
+  name: string;
+  phone: string;
+  email: string;
+  subject: string;
+  message: string;
+};
+
+export const createContactEntryService = async (
+  contactEntry: NewContactEntryType
+) => {
+  const { name, phone, email, subject, message } = contactEntry;
+
+  return await db.contactEntry.create({
+    data: {
+      name,
+      phone,
+      email,
+      subject,
+      message,
+    },
+  });
+};
